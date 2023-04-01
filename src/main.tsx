23ae7aa7dbf301c3513eddd7fe37bf6dc134b907
MRhuo857 <455801665@qq.com>
2023-04-01 15:26:54 +0800

    fix:增加右路，嵌套路由

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,31 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import App from "./App";
+import ErrorPage from "./components/ErrorPage";
 
-let id = document.getElementById('root') as HTMLElement;
-let root = ReactDOM.createRoot(id)
-
+const id = document.getElementById("root") as HTMLElement;
+const root = ReactDOM.createRoot(id);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Outlet />, //如果有一个可不写
+    errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <div>路由跳转</div> }, // 路由/显示
+      {
+        path: "1",
+        element: <div>路由1</div>,
+      },
+      {
+        path: "2",
+        element: <div>路由2</div>,
+      },
+    ],
+  },
+]);
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
