8c90e3f29e35d67502379fae421c99fe3bf2a2b5
MRhuo857 <455801665@qq.com>
2023-06-11 15:29:58 +0800

    feat:vite-plugin-mock实现本地mock

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
+import { UserConfigExport, ConfigEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import Unocss from 'unocss/vite'
+import { viteMockServe } from 'vite-plugin-mock'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [Unocss(),react()],
+  plugins: [Unocss(), react(), viteMockServe()],
 })
